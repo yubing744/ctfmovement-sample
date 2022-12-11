d2d06df337e88e9dd563686deca5d58f708712d5
Owen Wu <yubing744@163.com>
2022-12-11 08:45:27 +0800

    feat: get flag script

---
+++ scripts/01.create_account.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+aptos init --assume-yes --network custom --rest-url http://8.218.146.10:9080 --faucet-url http://8.218.146.10:9081
+
---
+++ scripts/02.get_flag.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+aptos move run --url http://8.218.146.10:9080 --function-id 0x21fb86005535eecc9d206182d26f0585600ad2150f780b9b19d2ad6a5f75a0b6::sample::get_flag
